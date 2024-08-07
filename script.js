95731d55454f0582ce6d7205ed45984ee11f00ac
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-08-07 21:51:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let search=document.querySelector(".search");
 let button=document.querySelector(".btn");
 let input=document.querySelector(".input")
 button.addEventListener("click",()=>{
-	search.active;
+	search.classList.add('active')
 	input.focus();
 })
